cc481f3e952ba2100e2cead70dbc61f2fd221e47
stimmerman <stimmerman@schubergphilis.com>
2020-10-22 16:09:15 +0200

    make instance_types a variable

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_eks_cluster" "default" {
 
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.default.name
-  instance_types  = ["t3.small"]
+  instance_types  = var.instance_types
   node_group_name = "default_node"
   node_role_arn   = aws_iam_role.default_node_group.arn
   subnet_ids      = var.subnet_ids
--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "log_retention" {
   description = "Retention of CloudWatch logs for the EKS cluster"
 }
 
+variable "instance_types" {
+  type        = list(string)
+  default     = null
+  description = "List of EC2 instance types to use for the worker nodes"
+}
+
 variable "name" {
   type        = string
   description = "Name of the cluster"
